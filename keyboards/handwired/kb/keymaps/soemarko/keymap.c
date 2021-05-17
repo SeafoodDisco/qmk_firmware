e6eb8b8e087426dcb7a3f116787cabbff93e61d2
soemarko <soemarko@users.noreply.github.com>
2021-05-17 18:42:02 +0700

    system login hotkey

--- keyboards/handwired/kb/keymaps/soemarko/keymap.c
+++ keyboards/handwired/kb/keymaps/soemarko/keymap.c
@@ -1,12 +1,16 @@
 #include "kb.h"
 
+enum custom_keycodes {
+    SR_LOGIN = SAFE_RANGE,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KEYMAP( // mute zoom, screenshot, prev song, play/payse, next song (or next layer when held), mute system (encoder click)
 		SGUI(KC_Z), SGUI(KC_5), KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, LT(1, KC_MEDIA_NEXT_TRACK), KC_MUTE),
 
 	KEYMAP( // reset (for flashing), escape, transparent, system sleep, transparent, transparent
-		RESET, KC_ESC, KC_TRNS, LGUI(LALT(KC_EJCT)), KC_TRNS, KC_TRNS),
+		RESET, KC_ESC, SR_LOGIN, LGUI(LALT(KC_EJCT)), KC_TRNS, KC_TRNS),
 
 	KEYMAP(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
@@ -59,7 +63,16 @@ void matrix_scan_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
+    switch (keycode) {
+        case SR_LOGIN:
+            if (record->event.pressed) {
+                SEND_STRING("asdf"); // put login password here
+            } else {
+                SEND_STRING(SS_TAP(X_ENTER));
+            }
+            break;
+    }
+    return true;
 }
 
 void encoder_update_user(int8_t index, bool clockwise) {
